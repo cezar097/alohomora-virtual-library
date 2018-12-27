18824cc0bdb721e108747e44f1c79c135136b856
Cezar Barbu <cezar97cta@gmail.com>
2018-12-27 21:39:46 +0000

    react build

---
+++ public/precache-manifest.2626cacf46a77243b00ca971ce0f203f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f65108e09c0c575e350c",
+    "url": "/static/js/main.f65108e0.chunk.js"
+  },
+  {
+    "revision": "1802201c6b69ebc1ccfc",
+    "url": "/static/js/1.1802201c.chunk.js"
+  },
+  {
+    "revision": "f65108e09c0c575e350c",
+    "url": "/static/css/main.042324aa.chunk.css"
+  },
+  {
+    "revision": "eb19bf95b1489a4c8649ddb61ae50fcc",
+    "url": "/index.html"
+  }
+];
